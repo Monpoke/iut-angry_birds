55b8773d7db3463ab575f3048995818e958b2876
Pierre Bourgeois <dragonralph@gmail.com>
2015-09-22 11:34:48 +0200

    Commit

--- src/angrybirds/Constantes.java
+++ src/angrybirds/Constantes.java
@@ -9,13 +9,9 @@ package angrybirds;
  *
  * @author Pierre
  */
-public class AngryBirds {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        
-    }
+public class Constantes {
+    
+    
+    
     
 }
