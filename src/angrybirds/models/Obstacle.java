ee9dac6ab9514b0ae36507ecf8b8d3ac6121ffc3
MoulardS <moulard.samuel@gmail.com>
2015-09-29 15:18:28 +0200

    Javadoc Obstacle | Samuel&Antoine

--- src/angrybirds/models/Obstacle.java
+++ src/angrybirds/models/Obstacle.java
@@ -9,6 +9,8 @@ import java.awt.Point;
 import javax.swing.JFrame;
 
 /**
+ * Crée un obstacle de forme rectangulaire avec lequel l'oiseau peut entrer en collision.
+ * 
  * 
  * @author Samuel - Antoine
  *
@@ -17,23 +19,50 @@ import javax.swing.JFrame;
 public class Obstacle extends Component{
 	
 	private Dimension dimension;
-	private Point centre;
+	private Point centre;	
+
+	/**
+	 * Constructeur
+	 */
+	public Obstacle() {}
 	
+	/**
+	 * Constructeur spécifiant le centre et la dimension
+	 * @param centre
+	 * @param dimension
+	 */
 	public Obstacle(Point centre, Dimension dimension) {
 		this.centre = centre;
 		this.dimension = dimension;
 	}
 	
-	public Obstacle() {}
-	
+	/**
+	 * @return la dimension de l'obstacle
+	 */
 	public Dimension getDimension() { return dimension;}
 	
+	/**
+	 * Donne une dimension à l'obstacle
+	 * @param dimension
+	 */
 	public void setDimension(Dimension dimension) { this.dimension = dimension;}
 	
+	/**
+	 * @return le centre de l'obstacle
+	 */
 	public Point getCentre() { return centre;}
 	
+	/**
+	 * Donne un centre à l'obstacle
+	 * @param point
+	 */
 	public void setCentre(Point point) {this.centre = centre;}
 	
+	/**
+	 * Dis si l'oiseau est entré en collision avec l'obstacle
+	 * @param bird
+	 * @return true si il y a collision, false sinon
+	 */
 	public boolean collides(Bird bird) {
 		
 		//En attente
@@ -41,6 +70,9 @@ public class Obstacle extends Component{
 		
 	}
 	
+	/**
+	 * Dessine l'obstacle
+	 */
 	public void paint (Graphics g) {
 		
 		g.drawRect((int) centre.getX(), (int) centre.getY(), (int) dimension.getWidth(), (int) dimension.getHeight());
