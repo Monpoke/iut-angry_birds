fa13353f2628f3b7523b856e1d97056fd621bfea
Pierre Bourgeois <dragonralph@gmail.com>
2015-10-06 14:00:27 +0200

    TrajectoireTest

--- src/angrybirds/tools/Trajectory.java
+++ src/angrybirds/tools/Trajectory.java
@@ -8,13 +8,18 @@ package angrybirds.tools;
  * @author Pierre
  */
 public abstract class Trajectory {
+ 
+    public static int parabolic(int x, int h) {
+        
+        double a = -0.09f;
+        double b = x/2f;
+        double c = x;
 
-    public static int parabolic(int x) {
+        double y = a * Math.pow(x, 2) + b * x + c;
 
-                
-        int y = 0;
-
-        return (int) y;
+        y = h -y;
+        
+        return (int) y / 120;
     }
 
 }
