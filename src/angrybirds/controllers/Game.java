56a1cb95316964de5f7c4a90612006ce6567ff1f
kevin MESSIAEN <messiaek@saule17.iut-infobio.priv.univ-lille1.fr>
2015-10-06 14:26:32 +0200

    Test sur les vecteur (fonction scale et perpendicular

--- src/angrybirds/controllers/Game.java
+++ src/angrybirds/controllers/Game.java
@@ -19,76 +19,76 @@ import angrybirds.tools.Trajectory;
 import angrybirds.views.Frame;
 
 /**
- *
+ * 
  * @author Pierre
  */
 public class Game {
 
-    private final Container contentPane;
+	private final Container contentPane;
 
-    private ArrayList<IDrawable> drawables = new ArrayList<>();
-    private Bird bird;
-    private final Frame frame;
-    private int startY;
+	private ArrayList<IDrawable> drawables = new ArrayList<>();
+	private Bird bird;
+	private final Frame frame;
+	private int startY;
 
-    /**
-     * Constructeur temporaire.
-     */
-    public Game() {
+	/**
+	 * Constructeur temporaire.
+	 */
+	public Game() {
 
-        frame = new Frame();
-        contentPane = frame.getContentPane();
+		frame = new Frame();
+		contentPane = frame.getContentPane();
 
-        this.addBird();
+		this.addBird();
 
-        frame.setVisible(true);
+		frame.setVisible(true);
 
-        // lance le refresh
-        this.refresh();
-    }
+		// lance le refresh
+		this.refresh();
+	}
 
-    /**
-     * La fonction ajoute un oiseau.
-     */
-    private void addBird() {
-        startY = (int) ((0.7 * frame.getDimension().getHeight()));
+	/**
+	 * La fonction ajoute un oiseau.
+	 */
+	private void addBird() {
+		startY = (int) ((0.7 * frame.getDimension().getHeight()));
 
-        bird = new Bird(new Vector(20, startY, 2, 1));
-        contentPane.add(new JComponent() {
-            @Override
-            public void paint(Graphics g) {
-                super.paint(g);
-                bird.draw(g);
-            }
-        });
-    }
+		bird = new Bird(new Vector(20, startY, 2, 1));
+		contentPane.add(new JComponent() {
+			@Override
+			public void paint(Graphics g) {
+				super.paint(g);
+				bird.draw(g);
+			}
+		});
+	}
 
-    /**
-     * Rafraîchit le content pane via un thread.
-     */
-    private void refresh() {
-        Thread t = new Thread(new Runnable() {
+	/**
+	 * Rafraîchit le content pane via un thread.
+	 */
+	private void refresh() {
+		Thread t = new Thread(new Runnable() {
 
-            @Override
-            public void run() {
-                while (true) {
-                    contentPane.repaint();
-                    Vector p = bird.getPosition();
+			@Override
+			public void run() {
+				while (true) {
+					contentPane.repaint();
+					Vector p = bird.getPosition();
 
-                    int x = (int) (p.getX() + 2);
-                    int y = (int) (Trajectory.parabolic(x, (int) p.getY())) + startY;
-                    System.out.println(y);
-                    bird.getPosition().setLocation(x, y);
-                    try {
-                        Thread.sleep(Constants.REFRESH_TIME);
-                    } catch (InterruptedException ex) {
-                        Logger.getLogger(Game.class.getName()).log(
-                                Level.SEVERE, null, ex);
-                    }
-                }
-            }
-        });
-        t.start();
-    }
+					int x = (int) (p.getX() + 2);
+					int y = (int) Trajectory.parabolic(x) + startY;
+					System.out.println(y);
+					bird.getPosition().setLocation(x, y);
+					try {
+						Thread.sleep(Constants.REFRESH_TIME);
+					} catch (InterruptedException ex) {
+						Logger.getLogger(Game.class.getName()).log(
+								Level.SEVERE, null, ex);
+					}
+				}
+			}
+		});
+		t.start();
+	}
 
 }
